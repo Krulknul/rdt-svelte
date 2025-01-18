a6ea12a2ab55ec1686ab805cac2a1bb555425e3e
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-01-18 17:18:20 +0100

    add some comments to example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    // When importing from another project, it'd be more like:
+    // import { ConnectButton } from "rdt-svelte";
     import ConnectButton from "$lib/ConnectButton.svelte";
     import { DataRequestBuilder } from "@radixdlt/radix-dapp-toolkit";
 
     // Set up a reactive state variable to hold the button instance
     let button: ConnectButton | undefined = $state();
 
+    // You can use $effect to use the button's api when it is ready.
+    // Of course, the button will only be ready after the ConnectButton component is mounted.
     $effect(() => {
         button?.rdt?.walletApi.setRequestData(
             DataRequestBuilder.accounts().exactly(1),
@@ -13,6 +17,7 @@
 </script>
 
 <div class="connect-button">
+    <!-- Use the component to put the button where you need it, and bind it to the reactive variable -->
     <ConnectButton bind:this={button} />
 </div>
 
